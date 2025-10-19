6d578ef9b4c3f6bd398cb7674d66af9e9c6ecd47
yvesSiewe <yvesrolandsiewe@gmail.com>
2025-10-19 16:59:44 +0100

    new

--- projet/semaine-1-layouts/tplayoutsbuilder/lib/main.dart
+++ projet/semaine-1-layouts/tplayoutsbuilder/lib/main.dart
@@ -1,79 +1,85 @@
-
 import 'package:flutter/material.dart';
-import 'composants/widget.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: ExampleCustomLayout(),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+class ExampleCustomLayout extends StatelessWidget {
+  const ExampleCustomLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      home: Layout(),
-    );
-  }
-}
-
-class Layout extends StatelessWidget{
-  const Layout({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return LayoutBuilder(
-      builder: (context, constraints){
-        if(constraints.maxWidth > 600){
-          double size = constraints.maxWidth*0.2;
-          double height = constraints.maxHeight;
-          return Scaffold(
-            body: SingleChildScrollView(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: size,
-                      child: Menu(sizedh: height,)
-                    ),
-                    SizedBox(width: 50,),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Header(sized: height*0.2),
-                          Body2(sized: height*0.7)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
-          );
-        }
-        else{
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Dashbord'),
-            ),
-            drawer: Drawer(),
-            body: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                   Header(),
-                  Body2()
-                ],
+    return Center(
+      child: CustomMultiChildLayout(
+        delegate: MyLayoutDelegate(),
+        children: [
+          LayoutId(
+            id: 'title',
+            child: Container(
+              color: Colors.blue,
+              padding: const EdgeInsets.all(8),
+              child: const Text(
+                'Titre',
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          );
-        }
-      }
+          ),
+          LayoutId(
+            id: 'icon',
+            child: Container(
+              width: 40,
+              height: 40,
+              color: Colors.red,
+              child: const Icon(Icons.star, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
+class MyLayoutDelegate extends MultiChildLayoutDelegate {
+  @override
+  void performLayout(Size size) {
+    // Taille du widget parent : "size"
+    // Positionnement de l'enfant avec id = 'title'
+    if (hasChild('title')) {
+      // On laisse Flutter calculer la taille optimale du child
+      Size titleSize = layoutChild(
+        'title',
+        BoxConstraints.loose(size),
+      );
 
+      // On positionne le titre en haut au centre
+      positionChild(
+        'title',
+        Offset((size.width - titleSize.width) / 2, 0),
+      );
+    }
+
+    // Positionnement de l'icône
+    if (hasChild('icon')) {
+      Size iconSize = layoutChild(
+        'icon',
+        BoxConstraints.tight(const Size(40, 40)), // taille fixe
+      );
+
+      // On place l'icône en bas à droite
+      positionChild(
+        'icon',
+        Offset(size.width - iconSize.width, size.height - iconSize.height),
+      );
+    }
+  }
+
+  @override
+  bool shouldRelayout(covariant MyLayoutDelegate oldDelegate) {
+    // Si la logique dépend de données, tu peux comparer ici
+    return false;
+  }
+}
