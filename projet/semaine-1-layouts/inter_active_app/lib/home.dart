e8bfb5f12d814f5b1fcb2e0de183e0d21e32aa40
Yves Siewe <yvesrolandsiewe@gmail.com>
2026-08-23 02:50:49 +0100

    modification du main

---
+++ projet/semaine-1-layouts/inter_active_app/lib/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget{
+  
+  const Homepage({super.key,});
+  
+  @override
+  State<Homepage> createState() {
+    // TODO: implement createState
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<Homepage>{
+
+  Color _backgroundColor = Colors.white;
+
+  final List<String> _titles = [
+    'Apprendre les interactifs',
+    'I love Flutter',
+    'East Sleep Code Repeat'
+  ];
+
+  int _titleIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titles[_titleIndex]),
+        centerTitle: true,
+        backgroundColor: _backgroundColor,
+      ),
+   
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: (){
+                setState((){
+                  _backgroundColor = (_backgroundColor == Colors.white) ? Colors.black : Colors.white;
+                });
+              }, 
+              
+              child: const Text('Cliquer Moi')
+            ),
+
+            ElevatedButton(
+              onPressed: (){
+                setState(() {
+                  _titleIndex = (_titleIndex + 1)%_titles.length;
+                });
+              }, 
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll<Color>(Colors.lightGreenAccent),
+                textStyle: WidgetStatePropertyAll<TextStyle>(TextStyle(fontSize: 20, color: Colors.white))
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.edit),
+                  Text('Changer le titre')
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
